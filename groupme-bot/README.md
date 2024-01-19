d5e269c07fdd6db21848bff9a3df133196ecd853
Shreya Pradhan <spradha3@terpmail.umd.edu>
2024-01-18 19:08:08 -0500

    First commit

---
+++ groupme-bot/README.md
@@ -0,0 +1,38 @@
+
+# Header: GroupMe Bot
+
+## Bot Features
+
+### 1. Greetings
+
+- Responds back to the user when the user says good morning or good night
+
+### 2. Personalized
+
+- The bot responds to everyone's greetings, but it only listens to the user's commands
+
+### 3. Rock-Paper-Scissors Game
+
+- Start a game session with the bot using "play" key phrase.
+- Could say "Let's play a game" or it could be as short as "play"
+- Make a choice! Enter r for rock, p for paper, or s for scissors
+- The bot's choice is random so there is no fixed winner
+- The winner is determined after the choices have been made
+- After each round, player can choose to continue playing or quit 
+
+## How to run the Bot
+
+1. **Run the Bot:**
+    - Open terminal in bot directory
+    - Run the bot using command 'python3 bot.py'
+
+2. **Interact with bot:**
+    - Send messages in the group to interact with the bot after running it
+
+3. **Commands:**
+    - Greetings: "good morning" or "good night" greetings
+    - Play game: Initiate with "play" or "play rock-paper-scissors"
+    - Respond: Respond to the user only  
+
+6. **Stopping bot:**
+    - Press 'Ctrl+c in terminal to stop
--- groupme-bot/bot.py
+++ groupme-bot/bot.py
@@ -3,6 +3,7 @@ import time
 import json
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
@@ -10,6 +11,7 @@ BOT_ID = os.getenv("BOT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 LAST_MESSAGE_ID = None
+PLAY_GAME = False
 
 
 def send_message(text, attachments=None):
@@ -37,14 +39,74 @@ def get_group_messages(since_id=None):
 def process_message(message):
     """Process and respond to a message."""
     global LAST_MESSAGE_ID
+    global PLAY_GAME
     text = message["text"].lower()
+    sender_id = message["sender_id"]
+    my_user_id = "105724506"
+    my_id = "883832"
+
+
+    #if the message is from itself or another bot, do not reply
+    if sender_id == my_id or message["sender_type"] == "bot":
+        return
 
     # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
-    if "hello bot" in text:
+    if sender_id == my_user_id and "hello" in text:
         send_message("sup")
 
+    elif sender_id == my_user_id and "hi" in text:
+        send_message("hello")
+
+    elif sender_id == my_user_id and "bye" in text:
+        send_message(f"Bye!")
+    
+    elif "good morning" in text:
+        sender_name = message.get("name", "Unknown")
+        send_message(f"Good morning, {sender_name}!")
+    
+    elif "good night" in text:
+        sender_name = message.get("name", "Unknown")
+        send_message(f"Good night, {sender_name}!")
+    
+    elif "play" in text:
+        send_message("Okay! Let's play Rock-Paper-Scissors\nPlease enter r for rock, p for paper, or s for scissors: ")
+        PLAY_GAME = True
+    
+    elif PLAY_GAME and text in ['r', 'p', 's']: 
+        user_entered = text
+        choices = ['rock', 'paper', 'scissors']
+        bot_choice = random.choice(choices)
+    
+        if user_entered == 'r':
+            user_choice = 'rock'
+        elif user_entered == 'p':
+            user_choice = 'paper'
+        elif user_entered == 's':
+            user_choice = 'scissors'
+        
+        send_message(f"Your choice: {user_choice}\nMy choice: {bot_choice}")
+        result = winner(user_choice, bot_choice)
+        send_message(f"{result}")
+
+        send_message("Would you like to continue? y/n")
+    elif PLAY_GAME and text in ['y', 'n']:
+        if text == 'n':
+            send_message("Let's play again some other time!")
+            PLAY_GAME = False
+        else:
+            send_message("Great! Let's play another round. \nEnter 'r' for rock, 'p' for paper, or 's' for scissors.")
+    
     LAST_MESSAGE_ID = message["id"]
 
+def winner(player, bot):
+    if player == bot:
+        return "We tied!"
+    elif (player == 'rock' and bot == 'scissors') or \
+         (player == 'paper' and bot == 'rock') or \
+         (player == 'scissors' and bot == 'paper'):
+         return "You win!"
+    else:
+        return "I win!"
 
 def main():
     global LAST_MESSAGE_ID
@@ -58,3 +120,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
